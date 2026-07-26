fb9196ba7899dfe3d519df8b055c10b7b98ee442
disss18 <ikomangyudis19@gmail.com>
2026-07-26 19:02:53 +0700

    Complete splash and onboarding flow

--- lib/ui/features/splash/splash_screen.dart
+++ lib/ui/features/splash/splash_screen.dart
@@ -9,19 +9,15 @@ class AnimatedSplashScreenWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AnimatedSplashScreen(
-      duration:
-          3000, // durasi splash tampil (ms), sesuaiin sama panjang animasi
-      splashIconSize: double
-          .infinity, // <- WAJIB, biar splash-nya gak dibatasi ukuran default package
+      duration: 3000,
+      splashIconSize: double.infinity,
       backgroundColor: Colors.black,
       splash: ClipRect(
         child: SizedBox.expand(
-          // <- full screen sesuai ukuran HP
           child: FittedBox(
-            fit: BoxFit.cover, // <- crop otomatis biar full, gak ada bar hitam
+            fit: BoxFit.cover,
             child: Transform.scale(
-              scale:
-                  1.1, // <- opsional, kalau mau crop dikit lagi dari full cover
+              scale: 1.1,
               child: Lottie.asset(
                 'assets/videos/splashscreen.json',
                 repeat: false,
